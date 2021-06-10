789b49cf069c1ef9ce1134330fca69ae4bc43849
unknown <ronaldmcorona@gmail.com>
2021-06-09 18:11:22 -0700

    building the tech stack: TS, Express, Svelte

---
+++ src/server/main.ts
@@ -0,0 +1,16 @@
+import express from 'express'
+import path from "path"
+
+const app = express()
+import { fileURLToPath } from 'url'
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const clientPath = path.join('src', 'client', 'public', 'index.html')
+const staticPath = path.join(__dirname, '..', '..', 'src', 'client', 'public')
+
+app.use(express.static(staticPath))
+
+app.get('/', (req, res) => res.send(path.join(staticPath, 'index.html')))
+
+app.listen(80, () => 'Server listening!')
+
+export {}
